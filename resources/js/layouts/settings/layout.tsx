a970f198c2d74e6efce57b69287d77749be591c9
seragasantri <seragasantri@gmail.com>
2026-02-27 06:08:51 +0700

    fix(settings): hapus fitur edit. profil, delete akun, dan two factory

--- resources/js/layouts/settings/layout.tsx
+++ resources/js/layouts/settings/layout.tsx
@@ -7,24 +7,12 @@ import { useCurrentUrl } from '@/hooks/use-current-url';
 import { cn, toUrl } from '@/lib/utils';
 import type { NavItem } from '@/types';
 import { edit as editAppearance } from '@/routes/appearance';
-import { edit } from '@/routes/profile';
-import { show } from '@/routes/two-factor';
 import { edit as editPassword } from '@/routes/user-password';
 
 const sidebarNavItems: NavItem[] = [
     {
-        title: 'Profile',
-        href: edit(),
-        icon: null,
-    },
-    {
         title: 'Password',
         href: editPassword(),
-        icon: null,
-    },
-    {
-        title: 'Two-Factor Auth',
-        href: show(),
         icon: null,
     },
     {
