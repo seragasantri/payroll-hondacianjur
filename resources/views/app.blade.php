c7293f4a42178e38bbe94f6754bf502af9f5957e
seragasantri <seragasantri@gmail.com>
2026-02-27 08:02:35 +0700

    feat(general): tambah favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,7 @@
 
     <link rel="icon" href="/assets/images/logo_potrait.png" sizes="any">
     <link rel="icon" href="/assets/images/logo_potrait.png" type="image/png+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="apple-touch-icon" href="/assets/images/logo_potrait.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
