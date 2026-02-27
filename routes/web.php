37c7c924b6d3f6c9bfa7e6bd2b9162e93c19c488
seragasantri <seragasantri@gmail.com>
2026-02-27 07:59:32 +0700

    fix(welcome): redirect / to /login

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::redirect('/', '/login')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
