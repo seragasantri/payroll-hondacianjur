ef3f5fb2bba9afbdff59e707f77f5aee23858db2
seragasantri <seragasantri@gmail.com>
2026-02-27 08:05:50 +0700

    feat(general): tambah favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,9 +33,11 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="icon" href="/assets/images/logo_potrait.png" sizes="any">
-    <link rel="icon" href="/assets/images/logo_potrait.png" type="image/png+xml">
-    <link rel="apple-touch-icon" href="/assets/images/logo_potrait.png">
+    <link rel="icon" type="image/png" href="/assets/images/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg" />
+    <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png" />
+    <link rel="manifest" href="/assets/images/site.webmanifest" />
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
